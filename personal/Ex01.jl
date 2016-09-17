bb4fa26f5ddf04fc5ad5f7b58f0a591675047a75
Unuzual <contato.pauloxavier@gmail.com>
2016-09-17 20:57:06 -0300

    Deletando diretorio antigo

--- personal/Ex01.jl
+++
@@ -1,5 +0,0 @@
-matrix = rand(6, 6)
-
-matrix_result = round(Int64, maximum(matrix, 2) .== matrix)
-
-println(matrix_result)
--- personal/Ex02.jl
+++
@@ -1,7 +0,0 @@
-matrix = [5 10 -5 22; 1 33 15 3; 8 29 12 1; 3 11 39 20]
-
-for i in 1 : 3
-    matrix[find(r->r==maximum(matrix), matrix)] = 0
-end
-
-println(matrix)
